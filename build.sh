5f318fdf61b48f250b54febcea9f2b4ceecbbd34
taku820801 <takuchanpower@gmail.com>
2023-06-23 13:56:19 +0900

    requirements.txt追加

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 # exit on error
 set -o errexit
 
-poetry export -f requirements.txt -o requirements.txt
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py superuser
